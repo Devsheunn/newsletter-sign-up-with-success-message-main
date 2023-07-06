bdbb9e67191cb655973caecfd02ee645db3ac04c
Devsheunn <emmanueloluwasheunn@gmail.com>
2023-07-06 12:05:34 +0100

    Finally fixed input bug

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ let confirmation = document.querySelector(".confirmation");
 
 //sucess message
 const success = () => {
-  console.log("sucess");
-  mainPage.classList.remove("mainpage");
   mainPage.classList.add("dont_display");
+  mainPage.classList.remove("mainpage");
   successCard.classList.remove("dont_display");
+  return true;
 };
 
 // reset input space
@@ -28,16 +28,27 @@ const reset = () => {
   submitBtn.removeEventListener("click", success);
 };
 
+//prevent enter key from refreshing the page and linking enter key to submit button
+input.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    submitBtn.click();
+  }
+});
+
 //funtion to validate email
-function validate() {
+function validate(e) {
   let reg = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+)(.[a-z]+)$/;
   if (!input.value.match(reg)) {
     submitBtn.removeEventListener("click", success);
+
     errorMessage.innerHTML = "Valid email required";
     errorMessage.style.color = "red";
+
     input.style.border = "1px solid rgb(255, 0, 0, 0.7)";
     input.style.backgroundColor = "rgb(255, 192, 203, 0.3)";
     input.style.color = "red";
+
     return false;
   }
 
@@ -50,7 +61,9 @@ function validate() {
   confirmation.innerHTML = ` A confirmation email has been sent to
      <strong>${input.value}</strong> Please open it and click the
      button inside to confirm your subscription.`;
+
   submitBtn.addEventListener("click", success);
+  return true;
 }
 
 //dismiss button to reset
